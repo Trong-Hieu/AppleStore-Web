16080bd12b06aa2a99660eaacf2ebbf7bf6ade31
ngtrhieu0303 <ngtrhieu0303@gmail.com>
2019-12-09 13:41:09 +0700

    Add check out html $ ts

--- src/app/check-out/check-out.component.ts
+++ src/app/check-out/check-out.component.ts
@@ -1,15 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { CartService } from '../cart.service';
+import { ShoppingCart } from '../models/shopping-cart';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-check-out',
   templateUrl: './check-out.component.html',
   styleUrls: ['./check-out.component.css']
 })
-export class CheckOutComponent implements OnInit {
+export class CheckOutComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  shipping = {};
+  cart: ShoppingCart;
+  subsciption: Subscription;
 
-  ngOnInit() {
+  constructor(
+    private cartService: CartService
+  ) { }
+
+  async ngOnInit() {
+    let cart$ = await this.cartService.getCart();
+    this.subsciption = cart$.subscribe(cart => this.cart = cart);
+  }
+
+  ngOnDestroy(){
+    this.subsciption.unsubscribe;
+  }
+
+  save (shipping){
+    console.log(shipping);
+    let order = {
+      datePlaced: new Date().getTime(),
+      shipping: this.shipping,
+      item: this.cart.items.map(i => {
+        return {
+          product: {
+            id: i.product.id,
+            title: i.product.title,
+            price: i.product.price,
+            category: i.product.category,
+            description: i.product.description,
+            imageUrl: i.product.imageUrl
+          },
+          quantity: i.quantity,
+          totalPrice: i.totalPrice
+        }
+      })
+    }
   }
 
 }
